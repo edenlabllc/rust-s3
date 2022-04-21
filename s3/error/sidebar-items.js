128b3b01e39f5eee51265faed601e83eb1382a14
durch <durch@users.noreply.github.com>
2022-04-21 22:13:07 +0000

    deploy: 32a5a69363cb74c86ed8b68f713ca14d8f5a2798

---
+++ s3/error/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["S3Error",""]]});
---
+++ source-files.js
@@ -0,0 +1,5 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["awscreds"] = {"name":"","files":["credentials.rs","error.rs","lib.rs"]};
+sourcesIndex["awsregion"] = {"name":"","files":["error.rs","lib.rs","region.rs"]};
+sourcesIndex["s3"] = {"name":"","files":["bucket.rs","bucket_ops.rs","command.rs","deserializer.rs","error.rs","lib.rs","request.rs","request_trait.rs","serde_types.rs","signing.rs","utils.rs"]};
+createSourceSidebar();
